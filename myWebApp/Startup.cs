47d67f935777ff78d2a27b6240d4007692f21a11
andro <anonyosmitra>
2020-08-31 16:42:05 +0530

    "A"

--- myWebApp/Startup.cs
+++ myWebApp/Startup.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Data.SqlClient;
+using static System.IO.StreamReader;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
 
 namespace myWebApp
 {
@@ -42,10 +47,36 @@ namespace myWebApp
             app.UseRouting();
 
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapPost("/postDetails", async context =>
+                {
+                    using StreamReader reader = new StreamReader (context.Request.Body);
+                    string body = await reader.ReadToEndAsync ();
+                    JObject req = JObject.Parse(body);
+                    string qr="insert into devotees([fName],[lName],[mobile],[city]) values(\'"+req["fName"]+"\',\'"+req["lName"]+"\',\'"+req["mobile"]+"\',\'"+req["city"]+"\')";
+                    Console.WriteLine(qr);
+                    string connetionString = null;
+                    SqlConnection cnn ;
+                    SqlCommand com ;
+                    connetionString = "Data Source=localhost;Initial Catalog=test;User ID=client;Password=client5";
+                    cnn = new SqlConnection(connetionString);
+                    try
+                    {
+                    cnn.Open();
+                    Console.WriteLine("Connection Open ! ");
+                    com = new SqlCommand(qr, cnn);
+                    com.ExecuteNonQuery();
+                    com.Dispose();
+                    cnn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                    Console.WriteLine("Can not open connection ! ");
+                    }
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
